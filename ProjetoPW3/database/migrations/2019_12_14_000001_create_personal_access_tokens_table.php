84a774a42e3f69f827c5379e07a557af68cbe367
HenriqueZkx <165193930+HenriqueZkx@users.noreply.github.com>
2024-04-03 14:31:52 -0300

    Update 2019_12_14_000001_create_personal_access_tokens_table.php

--- ProjetoPW3/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ ProjetoPW3/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -8,30 +8,23 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('contatos', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string("nome");
+            $table->string("telefone");
+            $table->string("email");
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('contatos');
     }
 };
